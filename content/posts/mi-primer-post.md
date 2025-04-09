2e02eb9166eb232f4ac70bf9002ae85d1a67d2a1
¨Pablito¨ <n8nlinux@gmail.com>
2025-04-09 15:40:52 -0400

    Primer post con imagen

---
+++ content/posts/mi-primer-post.md
@@ -0,0 +1,14 @@
++++
+date = '2025-04-09T14:34:49-04:00'
+draft = true
+title = 'Mi Primer Post hugo y Github'
++++
+
+Hola Mundooo!!!
+
+Aquì estoy aprendiendo junto a Gemini  y AI Studio Google sobre como crear un log personal en Github, sobre servidor Hugo y muchas cosas màs!
+
+https://aistudio.google.com/welcome/
+
+![screenshot construyendo entrada blog](/images/Screenshot_2025-04-09_15-01-12.png)
+
---
+++ public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Learning with IA",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Learning with IA \u003e  Posts",
+    "content": "Hola Mundooo!!!\nAquì estoy aprendiendo junto a Gemini y AI Studio Google sobre como crear un log personal en Github, sobre servidor Hugo y muchas cosas màs!\nhttps://aistudio.google.com/welcome/",
+    "description": "Hola Mundooo!!!\nAquì estoy aprendiendo junto a Gemini y AI Studio Google sobre como crear un log personal en Github, sobre servidor Hugo y muchas cosas màs!\nhttps://aistudio.google.com/welcome/",
+    "tags": [],
+    "title": "Mi Primer Post hugo y Github",
+    "uri": "/posts/mi-primer-post/index.html"
+  },
+  {
+    "breadcrumb": "Learning with IA",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "Learning with IA",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Learning with IA",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
