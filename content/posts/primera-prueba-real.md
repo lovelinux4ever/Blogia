7d7af6fc9aaa1b38450f7471c45649fb0f7c2839
¨Pablito¨ <n8nlinux@gmail.com>
2025-04-09 17:01:02 -0400

    Publicar post: Primera Prueba Real

---
+++ content/posts/primera-prueba-real.md
@@ -0,0 +1,13 @@
++++
+date = '2025-04-09T16:44:18-04:00'
+draft = false
+title = 'Primera Prueba Real'
++++
+Contenido de prueba! la primera prueba online!!
+
+Como se puede ver, gracias al servicio de Netlify, un blog online en menos de medio día! https incluido! No e fascinante lo que han creado y existe hoy en día? cualquiera con un navegador web puede llegar a leer esto!
+
+
+![screenshot primera prueba blog](/images/Screenshot_2025-04-09_16-47-54.png)
+
+Here we go!!!
--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -8,20 +8,28 @@ var relearn_searchindex = [
     "uri": "/index.html"
   },
   {
-    "breadcrumb": "Learning with IA \u003e  Posts",
-    "content": "Hola Mundooo!!!\nAquì estoy aprendiendo junto a Gemini y AI Studio Google sobre como crear un log personal en Github, sobre servidor Hugo y muchas cosas màs!\nhttps://aistudio.google.com/welcome/",
-    "description": "Hola Mundooo!!!\nAquì estoy aprendiendo junto a Gemini y AI Studio Google sobre como crear un log personal en Github, sobre servidor Hugo y muchas cosas màs!\nhttps://aistudio.google.com/welcome/",
-    "tags": [],
-    "title": "Mi Primer Post hugo y Github",
-    "uri": "/posts/mi-primer-post/index.html"
-  },
-  {
     "breadcrumb": "Learning with IA",
     "content": "",
     "description": "",
     "tags": [],
     "title": "Posts",
     "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "Learning with IA \u003e  Posts",
+    "content": "Contenido de prueba! la primera prueba online!!\nComo se puede ver, gracias al servicio de Netlify, un blog online en menos de medio día! https incluido! No e fascinante lo que han creado y existe hoy en día? cualquiera con un navegador web puede llegar a leer esto!\nHere we go!!!",
+    "description": "Contenido de prueba! la primera prueba online!!\nComo se puede ver, gracias al servicio de Netlify, un blog online en menos de medio día! https incluido! No e fascinante lo que han creado y existe hoy en día? cualquiera con un navegador web puede llegar a leer esto!\nHere we go!!!",
+    "tags": [],
+    "title": "Primera Prueba Real",
+    "uri": "/posts/primera-prueba-real/index.html"
+  },
+  {
+    "breadcrumb": "Learning with IA \u003e  Posts",
+    "content": "Hola Mundooo!!!\nAquì estoy aprendiendo junto a Gemini y AI Studio Google sobre como crear un log personal en Github, sobre servidor Hugo y muchas cosas màs!\nhttps://aistudio.google.com/welcome/",
+    "description": "Hola Mundooo!!!\nAquì estoy aprendiendo junto a Gemini y AI Studio Google sobre como crear un log personal en Github, sobre servidor Hugo y muchas cosas màs!\nhttps://aistudio.google.com/welcome/",
+    "tags": [],
+    "title": "Mi Primer Post hugo y Github",
+    "uri": "/posts/mi-primer-post/index.html"
   },
   {
     "breadcrumb": "Learning with IA",
